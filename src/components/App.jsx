3b291a02a32d82e959476f2228f13fec0dcd219e
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-10-16 23:07:33 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import Profile from './userProp'
+import user from './user.json'
+
+const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
-    </div>
+      
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+      quantity={user.stats.quantity}/>
+   </div>    
   );
 };
+
+export default App
